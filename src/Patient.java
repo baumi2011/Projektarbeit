cb7cb85154c97907ce2c35d7e0c3bfa435995ad9
baumi2011 <baumgartnerjjulian@gmx.at>
2024-12-05 14:56:26 +0100

    PatientenKlasse erstellt

---
+++ src/Patient.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.util.Date;
+
+public class Patient extends JFrame {
+
+    private int IDPatient;
+    private int SVNNummer;
+    private Date Geburtsdatum;
+    private String Wohnadresse;
+    private String Vorname;
+    private String Nachname;
+
+
+
+
+
+    public int getIDPatient() {
+        return IDPatient;
+    }
+
+    public void setIDPatient(int IDPatient) {
+        this.IDPatient = IDPatient;
+    }
+
+    public int getSVNNummer() {
+        return SVNNummer;
+    }
+
+    public void setSVNNummer(int SVNNummer) {
+        this.SVNNummer = SVNNummer;
+    }
+
+    public Date getGeburtsdatum() {
+        return Geburtsdatum;
+    }
+
+    public void setGeburtsdatum(Date geburtsdatum) {
+        Geburtsdatum = geburtsdatum;
+    }
+
+    public String getWohnadresse() {
+        return Wohnadresse;
+    }
+
+    public void setWohnadresse(String wohnadresse) {
+        Wohnadresse = wohnadresse;
+    }
+
+    public String getVorname() {
+        return Vorname;
+    }
+
+    public void setVorname(String vorname) {
+        Vorname = vorname;
+    }
+
+    public String getNachname() {
+        return Nachname;
+    }
+
+    public void setNachname(String nachname) {
+        Nachname = nachname;
+    }
+}

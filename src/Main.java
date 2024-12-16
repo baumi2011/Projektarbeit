d04cb0cfc436a230fcf6962c1787e2e9c7a1ac48
janhe <jan.hesch@edu.fh-joanneum.at>
2024-12-16 14:05:35 +0100

    Anfang Datenbank MySQL

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
     try {
         con = DriverManager.getConnection(URL, USER, PASSWORD);
         System.out.println("Connected to database");
-
+//Hallo
     }catch (SQLException e) {
         throw new RuntimeException(e);
     }

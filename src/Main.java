a20232286ae6364280ad521fbaad199681d0b5c7
baumi2011 <baumgartnerjjulian@gmx.at>
2024-12-16 14:25:04 +0100

    Finish Übung

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,7 @@ import java.sql.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/projektarbeit";
-    private static final String USER = "root";
-    private static final String PASSWORD = "janhesch501";
-
     public static void main(String[] args) {
-    Connection con = null;
-    try {
-        con = DriverManager.getConnection(URL, USER, PASSWORD);
-        System.out.println("Connected to database");
-//Hallo
-    }catch (SQLException e) {
-        throw new RuntimeException(e);
-    }
-
-
 
     SwingUtilities.invokeLater(() -> {
         Patient GUI = new Patient();
